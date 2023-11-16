109d7e72943d31babfe84c3add711c132b30ff36
Mariani Sfredo <mary_sfredo@hotmail.com>
2023-11-16 09:37:55 -0300

    login

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { SignupComponent } from './components/signup/signup.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgToastModule } from 'ng-angular-popup';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TokenInterceptor } from './interceptors/token.interceptor';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    NgToastModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+
+  ],
+  providers: [{
+    provide:HTTP_INTERCEPTORS,
+    useClass:TokenInterceptor,
+    multi:true
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,68 @@
+import { AuthService } from './../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from '../../helpers/validationform';
+import { Router } from '@angular/router';
+import { MatDialogModule } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  public signUpForm!: FormGroup;
+  type: string = 'password';
+  isText: boolean = false;
+  eyeIcon:string = "fa-eye-slash";
+  loading:boolean=true;
+  constructor(private fb : FormBuilder, private auth: AuthService, private router: Router, MatDialogModule: MatDialogModule) { }
+
+  ngOnInit() {
+    this.signUpForm = this.fb.group({
+      firstName:['', Validators.required],
+      lastName:['', Validators.required],
+      userName:['', Validators.required],
+      email:['', Validators.required],
+      password:['', Validators.required]
+    })
+  }
+
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = 'fa-eye' : this.eyeIcon = 'fa-eye-slash'
+    this.isText ? this.type = 'text' : this.type = 'password'
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+          console.log(this.signUpForm.value);
+      let signUpObj = {
+        ...this.signUpForm.value,
+        role:'',
+        token:''
+      }
+      this.loading = true; 
+      this.auth.signUp(signUpObj)
+      .subscribe({
+        next:(res=>{
+          console.log(res.message);
+          this.signUpForm.reset();
+          this.router.navigate(['login']);
+          alert(res.message)
+        }),
+        error:(err=>{
+          alert(err?.error.message)
+        }),
+        complete: () => {
+          this.loading = false; 
+        }
+      })
+    } else {
+      ValidateForm.validateAllFormFields(this.signUpForm); //{7}
+      this.loading = false;
+    }
+  }
+
+}
